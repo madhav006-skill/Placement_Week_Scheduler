fa809a6488bddd779c1762cf65efddd8b2e3ae05
madhav006-skill <amankumar552023@gmail.com>
2026-08-26 19:47:17 +0530

    fix: use routes in vercel.json with clean api index

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const app = require('../backend/src/index');
+
+module.exports = (req, res) => {
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + req.url;
+  }
+  return app(req, res);
+};
