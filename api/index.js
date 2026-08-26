bb17f58d303b3398353b940e7f08ce95951da4b6
madhav006-skill <amankumar552023@gmail.com>
2026-08-26 20:22:28 +0530

    fix: use native URL to parse query string in lambda to prevent crash

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const app = require('../backend/src/index');
 
 module.exports = (req, res) => {
-  if (req.query && req.query.path) {
-    req.url = '/api/' + req.query.path;
+  // Extract path from query string natively
+  const urlObj = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
+  const pathParam = urlObj.searchParams.get('path');
+  
+  if (pathParam) {
+    req.url = '/api/' + pathParam;
   }
   return app(req, res);
 };
